ba43ae7206c624af089e3667ef14dea7d5971e6b
little-buddy <litoubuddy@gmail.com>
2020-08-25 09:33:17 +0800

    update doc

--- README.md
+++ README.md
@@ -1,39 +1,29 @@
-# ERC20 token tempalte
+# ERC20 token template
+
+该文档是一份在 `ganache 客户端` 下简单发布 ERC20 token 的教程
+按以下步骤操作前，请确定已经安装了 [ganache](https://www.trufflesuite.com/ganache) 客户端并启动。
 
 ```
-该文档是一份在 ganache 客户端下简单发布 ERC20 token 的教程
-
-在按以下步骤操作的时候，请确定已经安装了 ganache 客户端并启动
-
-所有配置都是默认配置，不需要做调整
+git clone git@github.com:little-buddy/ERC20_demo.git
 ```
 
 ```
-git clone xxx@git...
-
-npm install
-or
-yarn
-
+npm install / yarn
 ```
 
-- Frist
-
 ```
-  npm i -g truffle
+npm i -g truffle
 ```
-
-- Second
 
 ```
 truffle migrate
 
-如果发现此过程较为漫长，是truffle在下载0.6.0+版本的编译器，无需惊慌请耐心等待一会即可
+如果发现此过程较为漫长，是truffle在下载 solc@0.6.8 的编译器，无需惊慌请耐心等待一会即可
 ```
 
 ```
-现在你可以基于一些sdk进行响应代币操作的调试了
+现在你可以基于一些sdk进行相应代币操作的调试了
 
-默认rpc http:127.0.0.1:7545
+默认rpc http://127.0.0.1:7545
 
 ```
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require("fs");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraKey = fs.readFileSync(".infurakey").toString().trim();
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -43,9 +42,9 @@ module.exports = {
     // options below to some value.
     //
     // development: {
-    //   host: "127.0.0.1", // Localhost (default: none)
-    //   port: 8545, // Standard Ethereum port (default: none)
-    //   network_id: "*", // Any network (default: none)
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
     // },
     // Another network with more advanced options...
     // advanced: {
@@ -59,12 +58,16 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    //   provider: () =>
+    //     new HDWalletProvider(
+    //       mnemonic,
+    //       `https://ropsten.infura.io/v3/${infuraKey}`
+    //     ),
+    //   network_id: 3, // Ropsten's id
+    //   gas: 5500000, // Ropsten has a lower block limit than mainnet
+    //   confirmations: 2, // # of confs to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     // },
     // Useful for private networks
     // private: {
